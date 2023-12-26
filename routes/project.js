9ca05281bdbde36c22322f8b021910fb6f33d33d
lishayst100 <lishayst100@gmail.com>
2023-12-26 14:42:43 +0200

    temp

--- routes/project.js
+++ routes/project.js
@@ -9,7 +9,7 @@ const router = Router()
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/');
+      cb(null, '/tmp');
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now();
