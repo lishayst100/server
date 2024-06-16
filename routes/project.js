470689a3d5d6680bbb7dab65a6209ab88198b5de
lishayst100 <lishayst100@gmail.com>
2024-06-16 17:58:48 +0300

    wuth correct path

--- routes/project.js
+++ routes/project.js
@@ -8,7 +8,7 @@ const router = Router()
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../public');
+    cb(null, './public');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
